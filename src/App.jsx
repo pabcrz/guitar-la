f38f8db9b1ef5b496fb28a26c716baad1a536afb
Pablo Cruz <pcruzp1400@gmail.com>
2024-10-18 04:35:22 -0600

    Add initial structure, Guitar and Header components

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+import { db } from "./data/db";
+
+function App() {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    setCart((prevCart) => [...prevCart, item]);
+  }
+  return (
+    <>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar, i) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
